79983fc0f6686657f029ce7a0727dec4d55ac591
taninatti <taninatti@yahoo.co.jp>
2018-06-20 01:39:58 +0900

    Lesson4 課題
    Testに応用課題、Assighmentに課題を入れました。

---
+++ Assets/Test.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Boss : MonoBehaviour
+{
+    private int hp = 100;
+    private int power = 25;
+    private int mp = 53;
+
+    public void Attack()
+    {
+        Debug.Log(this.power + "のダメージを与えた");
+    }
+
+    public void Defence(int damage)
+    {
+        Debug.Log(damage + "のダメージを受けた");
+        this.hp -= damage;
+    }
+    public void Magic(int magic)
+    {
+        this.mp = this.mp - magic;
+        Debug.Log("魔法攻撃をした。残りMPは"+ this.mp);
+        for (int i = 0; i < 10; i++)
+        {
+            this.mp = this.mp - magic;
+            if (mp < 5)
+            {
+                Debug.Log("MPが足りないため魔法が使えない。");
+            }
+        }
+    
+    }
+}
+
+public class Test : MonoBehaviour
+{
+
+    void Start()
+    {
+        Boss lastboss = new Boss();
+
+        lastboss.Attack();
+        lastboss.Defence(3);
+        lastboss.Magic(5);
+    }
+}
+
+
+
+
+
+
+
+
---
+++ Assets/assighment.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class assighment : MonoBehaviour
+{
+    void Start()
+    {
+        int[] array = new int[5];
+        array[0] = 1 * 10;
+        array[1] = 2 * 10;
+        array[2] = 3 * 10;
+        array[3] = 4 * 10;
+        array[4] = 5 * 10;
+
+        for (int i = 0; i < 5; i++)
+        {
+            Debug.Log(array[i]);
+        }
+        for (int i = 4; i >= 0; i--)
+        {
+            Debug.Log(array[i]);
+        }
+    }
+    void Update()
+    {
+
+    }
+}
+
